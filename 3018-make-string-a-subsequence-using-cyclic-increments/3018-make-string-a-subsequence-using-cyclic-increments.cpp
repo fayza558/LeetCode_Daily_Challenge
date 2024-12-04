fbfc2b78ae363bc274640abd35fa33e1bcc91fba
fayza mahmoud <110182701+fayza558@users.noreply.github.com>
2024-12-04 20:39:10 +0200

    Time: 3 ms (66.85%), Space: 17.1 MB (13.81%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -2,24 +2,30 @@ class Solution {
 public:
     bool canMakeSubsequence(string str1, string str2) {
    
-        int i=0,j=0,n=str1.size(),m=str2.size();
-        while(i<n&&j<m)
-        {
-            char nxt =str1[i];
-            nxt++;
-            if(nxt>'z')nxt='a';
-            if(str1[i]==str2[j])
-            {
-                i++;
-                j++;
-            }
-           else if(nxt==str2[j])
-            {
-                i++;
-                j++;
-            }else i++;
-        }
-        return j==m ;
+        // int i=0,j=0,n=str1.size(),m=str2.size();
+        // while(i<n&&j<m)
+        // {
+        //     char nxt =str1[i];
+        //     nxt++;
+        //     if(nxt>'z')nxt='a';
+        //     if(str1[i]==str2[j])
+        //     {
+        //         i++;
+        //         j++;
+        //     }
+        //    else if(nxt==str2[j])
+        //     {
+        //         i++;
+        //         j++;
+        //     }else i++;
+        // }
+        // return j==m ;
+     int j = 0;
+        for(char c : str1)
+            j+=(c == str2[j] || c-str2[j] == 25 | c-str2[j]==-1 );
+        return j == str2.size();
         
     }
+        
+    
 };
