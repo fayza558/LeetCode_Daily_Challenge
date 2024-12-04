973884a28627001a27f5b4d592f389705bb6b770
fayza mahmoud <110182701+fayza558@users.noreply.github.com>
2024-12-04 20:35:06 +0200

    Time: 16 ms (7.18%), Space: 17.2 MB (7.53%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -2,12 +2,24 @@ class Solution {
 public:
     bool canMakeSubsequence(string str1, string str2) {
    
-        int j=0;
-        for(char c: str1)
+        int i=0,j=0,n=str1.size(),m=str2.size();
+        while(i<n&&j<m)
         {
-            j+=(c ==str2[j]||c-str2[j]==25||c-str2[j]==-1);
+            char nxt =str1[i];
+            nxt++;
+            if(nxt>'z')nxt='a';
+            if(str1[i]==str2[j])
+            {
+                i++;
+                j++;
+            }
+           else if(nxt==str2[j])
+            {
+                i++;
+                j++;
+            }else i++;
         }
-        return j==str2.size();
+        return j==m ;
         
     }
 };
