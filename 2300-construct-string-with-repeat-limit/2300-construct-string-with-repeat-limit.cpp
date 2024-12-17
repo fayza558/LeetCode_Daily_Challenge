114107e880e2ec0f1eb9501a1e46ac796f57fdeb
fayza mahmoud <110182701+fayza558@users.noreply.github.com>
2024-12-17 22:08:08 +0200

    Time: 16 ms (88.81%), Space: 25.8 MB (95.24%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
@@ -6,23 +6,23 @@ public:
         vector<int>v;
         for(int i=0;i<26;i++) 
            if(freq[i])v.push_back('a'+i);
-        string ans;
+        s="";
         while(v.size())
         {
             int lst=v.back()-'a';
             v.pop_back();
             int minn=min(repeatLimit,freq[lst]);
             freq[lst]-=minn;
-            ans+=string(minn,lst+'a');
+            s+=string(minn,lst+'a');
             if(v.size()&&freq[lst])
             {
                 int cur=v.back()-'a';
-                ans.push_back(cur+'a');
+                s.push_back(cur+'a');
                 freq[cur]--;
                 if(freq[cur]==0) v.pop_back();
                 v.push_back(lst+'a');
             }
         }
-        return ans;
+        return s;
     }
 };
