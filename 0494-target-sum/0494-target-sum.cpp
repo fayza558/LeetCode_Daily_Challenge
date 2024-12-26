e3eee735cb14feb1e71f92f0504b38b614c41aa8
fayza mahmoud <110182701+fayza558@users.noreply.github.com>
2024-12-26 21:53:43 +0200

    Time: 7 ms (71.97%), Space: 11.8 MB (64.33%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
- //brute force
+    int dp[21][3001];
+
     int solve(int idx,int target ,vector<int>& nums){
-        if(idx==nums.size()) return !target;
-       return  solve(idx+1,target+nums[idx],nums)+ solve(idx+1,target-nums[idx],nums);
+        if(idx==nums.size()) return target==1000;
+        if(~dp[idx][target])return dp[idx][target];
+       return  dp[idx][target]= solve(idx+1,target+nums[idx],nums)+ solve(idx+1,target-nums[idx],nums);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(0,target,nums);
+        memset(dp,-1,sizeof dp);
+        return solve(0,target+1000,nums);
     }
 };
