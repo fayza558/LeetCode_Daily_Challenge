e5f183d5d9c6e71e4b946f86deb4cc3b3242b5d4
fayza mahmoud <110182701+fayza558@users.noreply.github.com>
2025-01-09 22:31:44 +0200

    Time: 27 ms (5.09%), Space: 63.9 MB (5%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -1,9 +1,31 @@
+struct TrieNode {
+TrieNode* child[26];
+int freq=0;
+}*trie;
 class Solution {
 public:
+    void insert(string &s){
+       TrieNode *top =trie;
+       for(char ch:s){
+          if(top->child[ch-'a']==nullptr)top->child[ch-'a']=new TrieNode();
+          top=top->child[ch-'a'];
+          top->freq++;
+       }
+    }
+    int search(string &s){
+       TrieNode *top =trie;
+       for(char ch:s){
+          if(top->child[ch-'a']==nullptr)return 0;
+          top=top->child[ch-'a'];
+     
+       }
+       return top->freq;
+    }
     int prefixCount(vector<string>& words, string pref) {
-        int cntr=0;
-        for(string word:words)
-             cntr+=word.substr(0,pref.size())==pref;
-        return cntr;
+        trie=new TrieNode();
+        for(string word:words){
+            insert(word);
+        }
+        return search(pref);
     }
 };
