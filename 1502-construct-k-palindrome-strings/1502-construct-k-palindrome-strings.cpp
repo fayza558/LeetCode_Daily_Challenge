9913df3adedcc3e72cadcc643059b44007bcad10
fayza mahmoud <110182701+fayza558@users.noreply.github.com>
2025-01-12 19:53:57 +0200

    Time: 3 ms (68%), Space: 14.9 MB (50.15%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     bool canConstruct(string s, int k) {
         if(k>s.size())return false;
-        vector<int>freq(26);
-        int odd=0;
-        for(char ch:s)freq[ch-'a']++;
-        for(int i=0;i<26;i++)
-           if(freq[i]&1)
-             odd++;
-        return odd<=k;
-        
+        int freq=0;
+        for(char ch:s)
+        freq^=(1<<(ch-'a'));
+        return __builtin_popcount(freq)<=k;
     }
 };
